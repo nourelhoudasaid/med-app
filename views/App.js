90dcc9ec040f5e014bf14284400ba1fa7bab3629
nour el houda said <77454579+nourelhoudasaid@users.noreply.github.com>
2024-10-16 00:41:49 +0200

    workkk

---
+++ views/App.js
@@ -0,0 +1,23 @@
+// /App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Register from './components/Register';
+import Login from './components/Login';
+
+
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          { /*more routes  */}
+        </Routes>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
+
